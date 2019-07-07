9a03708afff35fbe46df42898bd43173a90a995c
Iain Davis <iain@localhost.localdomain>
2019-07-07 16:43:13 -0700

    adding separate folder for profile-only functions and creating init scripts

--- init.d/init_bash.sh
+++ init.d/init_bash.sh
@@ -1,7 +1,9 @@
-ln -s ./bash/bash.d ~/.bash.d
-ln -s ./bash/bash.profile.d ~/.bash.profile.d
-ln -s ./bash/bash.d.init ~/.bash.d.init
-ln -s ./bash/bash.profile.d.init ~/.bash.profile.d.init
+ln -s $(pwd -P)/bash/bash.d ~/.bash.d
+ln -s $(pwd -P)/bash/bash.profile.d ~/.bash.profile.d
+ln -s $(pwd -P)/bash/bash.d.init ~/.bash.d.init
+ln -s $(pwd -P)/bash/bash.profile.d.init ~/.bash.profile.d.init
 
 echo 'source ~/.bash.d.init' >> ~/.bashrc
 echo 'source ~/.bash.profile.d.init' >> ~/.bash_profile
+
+echo 'export PATH=~/.dev/bin:$PATH' >> ~/.bashrc
--- init.d/init_vim.sh
+++ init.d/init_vim.sh
@@ -1,2 +1,3 @@
-ln -s vim/vim_root ~/.vim
-ln -s vim/vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s $(pwd -P)/vim/vim_root ~/.vim
+ln -s $(pwd -P)/vim/vimrc ~/.vimrc
